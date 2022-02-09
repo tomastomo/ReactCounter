a6c51a90bb9fc35ff5b2ca9427199dff1cb109f6
Tomàs Tortosa Moltó <tomastormol@gmail.com>
2022-02-09 17:11:47 +0100

    Counter

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Navbar</span>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
